efce1335457cac1eb23f3777efc70958a30ba482
Santiago Martinez <santiago.martinez.95@hotmail.com>
2021-11-23 20:47:53 -0500

    Se agregaro el reto con cupones

--- descuentos.js
+++ descuentos.js
@@ -29,3 +29,45 @@ function onclickbuttonpricediscount(){
     const result = document.getElementById("resultprice");
     result.innerText = "El precio con descuento son: $" + preciocondescuento
 };
+
+///Array con cupones
+
+const cupones = [
+    "ABCD",
+    "EFGH",
+    "IJKML",
+    ];
+
+function onclickbuttoncupondescuento(){
+
+    const inputprice = document.getElementById("inputprice");
+    const inputcupon = document.getElementById("inputcupon");
+    
+    const cuponvalue = inputcupon.value;
+    const pricevalue = inputprice.value;
+
+let descuento; 
+
+switch(cuponvalue) {
+    case cupones [0]: descuento = 15;
+
+    break;
+    
+    case cupones [1]: descuento = 30;
+
+    break;
+
+    case cupones [2]: descuento = 25;
+
+    break;
+}
+
+
+
+
+const precioconcupon = calcularpreciocondescuento (pricevalue, descuento);
+
+const resultado = document.getElementById("resultadocupon");
+resultado.innerText = "Cupon valido por un " + descuento + "% de descuento."+ " El precio con descuento son $" + precioconcupon;
+
+};
