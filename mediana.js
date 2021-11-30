2ab41a8ebccc686e677686147abeca567952a416
Santiago Martinez <santiago.martinez.95@hotmail.com>
2021-11-30 18:32:19 -0500

    se finalizo la actividad de mediana, incluyendo el sort de forma general

--- mediana.js
+++ mediana.js
@@ -1,23 +1,45 @@
-const lista1 = [
-    100,
-    200,
-    500,
-    400000000];
+function calcularmediana (lista){
+    function calcularmediaaritmetica(lista){
+      const sumalista = lista.reduce(
+          function(valoracumulado = 0, nuevoelemento){
+              return valoracumulado + nuevoelemento;
+          }
+      );
+      const promediolista = sumalista / lista.length;
+      
+      return promediolista;
+      
+      };
+    
+    lista.sort();
+      
+    const mitadlista = parseInt(lista.length / 2);
 
-const mitadlista1 = parseInt(lista1.length / 2);
+    function espar (numero){
+        if (numero % 2 === 0){
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-function espar (numero){
-    if (numero % 2 === 0){
-        return true;
+    let mediana;
+
+    if (espar(lista.length)) {
+        const elemento1 = lista[mitadlista - 1];
+        const elemento2 = lista[mitadlista];
+
+        const promedioelemento1y2 = calcularmediaaritmetica([
+            elemento1, 
+            elemento2]
+            );
+        
+    mediana = promedioelemento1y2
+
     } else {
-        return false;
-    }
+        mediana = lista[mitadlista];
+    };
+
+    return mediana;
+    
 };
-
-let mediana;
-
-if (espar(lista1.length)) {
-
-} else {
-    mediana = lista1 [mitadlista1];
-}
